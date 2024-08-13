d9dabcac081fe9a90061a32a922a42de71f9c048
iraryc <111704084+iraryc@users.noreply.github.com>
2024-08-13 11:45:33 -0700

    work on css and added more to the homepage
    work on css and added more to the homepage

---
+++ script/login.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const loginForm = document.querySelector("#login");
+
+loginForm.onsubmit = function (event) {
+    // Prevent the form from refreshing the page,
+    // as it will do by default when the Submit event is triggered:
+    event.preventDefault();
+
+    // We can use loginForm.username (for example) to access
+    // the input element in the form which has the ID of "username".
+    const loginData = {
+        username: loginForm.username.value,
+        password: loginForm.password.value,
+    }
+
+    // Disables the button after the form has been submitted already:
+    loginForm.loginButton.disabled = true;
+
+    // Time to actually process the login using the function from auth.js!
+    login(loginData);
+};
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".custom-container");
+
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove("sign-up-mode");
+});
+
+
+// New code for signup form submission handling
+const signupForm = document.querySelector("#signup");
+
+signupForm.onsubmit = function (event) {
+    event.preventDefault();
+
+    const signupData = {
+        username: signupForm.username.value,
+        fullname: signupForm.fullname.value,
+        password: signupForm.password.value,
+    };
+
+    signupForm.signUpButton.disabled = true;
+
+    signup(signupData).then(() => {
+        signupForm.signUpButton.disabled = false;
+    });
+};
+
+function scrollToSignup() {
+    const signupSection = document.getElementById('signup-section');
+    signupSection.scrollIntoView({ behavior: 'smooth' });
+}
