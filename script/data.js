47e9507d6f1884578d744e7fcffd7915b3ecdd1a
iraryc <111704084+iraryc@users.noreply.github.com>
2024-09-23 07:29:16 -0700

    updated data
    updated data

--- script/data.js
+++ script/data.js
@@ -2,5 +2,5 @@ function logout() {
     // Example: Clear user session or token
     sessionStorage.clear(); // or localStorage.clear();
     // Redirect to login page or home
-    window.location.href = '/index.html';
+    window.location.href = './index.html';
 }
